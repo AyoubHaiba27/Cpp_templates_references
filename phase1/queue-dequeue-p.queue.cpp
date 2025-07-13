eaae09221516e6bcc704977b7cce154d6b932d4b
Ayoub Haiba <ayoubgaberhaiba@gmail.com>
2025-07-13 17:07:59 +0300

    queue-dequeue-p.queue.cpp
    Signed-off-by: Ayoub Haiba <ayoubgaberhaiba@gmail.com>

---
+++ phase1/queue-dequeue-p.queue.cpp
@@ -0,0 +1,39 @@
+//queue
+#include<queue>
+
+queue<data_type>queue_name;//declear a queue
+
+queue_name.push(var);//pushes var to the end of of the queue
+queue_name.pop();//pops the first element of the queue
+queue_name.front();//returns the first element in the queue
+queue_name.back();//returns the last element in the queue
+queue_name.size();//returns the size of the queue
+queue_name.empty();//returns either the queue is empty(1) or not(0)
+
+//dequeue
+#include<dequeue>
+
+dequeue<data_type>dequeue_name(dequeue_size);//declear a dequeue
+
+dequeue_name[i]//access the element in the position i
+  
+dequeue_name.push_back(var);//pushes var to the end of of the dequeue
+dequeue_name.push_front(var);//pushes var to the start of of the dequeue
+dequeue_name.pop_front();//pops the first element of the dequeue
+dequeue_name.pop_back();//pops the last element of the dequeue
+dequeue_name.size();//returns the size of the dequeue
+dequeue_name.empty();//returns either the dequeue is empty(1) or not(0)
+dequeue_name.clear();//erases all elements in the dequeue
+
+//priority queue
+#include<queue>
+
+priority_queue<data_type>priority_q_name;//declear a priority queue ascendingly
+priority_queue<data_type, vector<data_type>, greater<data_type>>priority_q_name;//declear a priority queue ascendingly // all three fata types must be the same
+
+priority_q_name.push(var);//pushes var to its place in the priority queue
+priority_q_name.pop();//pops the first element of the priority queue
+priority_q_name.top();//returns the first element in the priority queue
+priority_q_name.size();//returns the size of the priority queue
+priority_q_name.empty();//returns either the priority queue is empty(1) or not(0)
+
