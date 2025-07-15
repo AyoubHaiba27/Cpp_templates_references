7e50c4861d2e79f4ea1557ffe2ab7f50183af293
Ayoub Haiba <ayoubgaberhaiba@gmail.com>
2025-07-15 16:12:41 +0300

    stack.cpp
    Signed-off-by: Ayoub Haiba <ayoubgaberhaiba@gmail.com>

--- phase1/stack.cpp
+++ phase1/stack.cpp
@@ -7,3 +7,7 @@ stack_name.pop();//pops the top element of the stack
 stack_name.top();//returns the top element in the stack
 stack_name.size();//returns the size of the stack
 stack_name.empty();//returns either the stack is empty(1) or not(0)
+
+/*
+stack worksin first in last out
+*/
