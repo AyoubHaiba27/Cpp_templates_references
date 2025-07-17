c119d7fae55177a7624730f0a61b2bacc5d159f7
Ayoub Haiba <ayoubgaberhaiba@gmail.com>
2025-07-17 18:52:53 +0300

    set.cpp
    Signed-off-by: Ayoub Haiba <ayoubgaberhaiba@gmail.com>

--- phase1/set.cpp
+++ phase1/set.cpp
@@ -6,8 +6,8 @@ set_name.insert(val);//insert val to set
 set_name.erase(val);//erases val from set  //function takes both iterator or value
 set_name.find(val);//returns an iterator pointing on val if exisits, if not returns an iterator pointing on the end of the set (s.end())
 set_name.size();//returns size of set
-
-
+set_name.lower_bound(x);//returns an iterator pointing to the first element greater than or equal x
+set_name.upper_bound(x);//returns an iterator pointing to the first element greater than x
 
 
 //The set is a sorted array without repitition
