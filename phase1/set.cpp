bf203cc5353e98dd89466cf6c813bd293b544580
Ayoub Haiba <ayoubgaberhaiba@gmail.com>
2025-07-17 18:15:13 +0300

    set.cpp
    Signed-off-by: Ayoub Haiba <ayoubgaberhaiba@gmail.com>

---
+++ phase1/set.cpp
@@ -0,0 +1,17 @@
+#include<set>
+
+set<data_type> set_name;//declear a set
+
+set_name.insert(val);//insert val to set
+set_name.erase(val);//erases val from set  //function takes both iterator or value
+set_name.find(val);//returns an iterator pointing on val if exisits, if not returns an iterator pointing on the end of the set (s.end())
+set_name.size();//returns size of set
+
+
+
+
+//The set is a sorted array without repitition
+
+//multi set
+multiset<data_type> set_name;//declear a multi set
+//multi set is the same as set except it has repetitions 
